650178e860009b4864eb20416021d427b5997f6a
Astghik1319 <74616021+Astghik1319@users.noreply.github.com>
2021-06-03 17:32:47 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
-
+mail = Mail(app)
 
 @app.route("/")
 def home():
@@ -32,3 +34,25 @@ def not_found_error(error):
 if __name__ == '__main__':
 
     app.run(debug=True)
+
+
+)
+
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'yourId@gmail.com', recipients = ['someone1@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
