24d0b40e3e2a1c879c8b413f91c61c033218754c
Astghik1319 <74616021+Astghik1319@users.noreply.github.com>
2021-06-08 19:30:38 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -1,93 +1,94 @@
 from flask import Flask
 from flask import render_template
-from flask import request
-from flask import abort, send_from_directory
+from flask_mail import Mail, Message
+from flask import Flask, render_template, request
 from AzureDB import AzureDB
-from datetime import datetime
-import os
+import mysql.connector
+from mysql.connector import errorcode
+
 app = Flask(__name__)
+mail = Mail(app)
 
-
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/gallery')
-def gallery():
-    return render_template('gallery.html')
-
-
-@app.route('/aboutme')
+@app.route("/aboutme")
 def aboutme():
-    return render_template('aboutme.html')
+    return render_template("aboutme.html")
 
 
-@app.route('/contact')
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
 
 
-@app.route('/guestbook')
-def guestbook():
-    with AzureDB() as a:
-        data = a.azureGetData()
-        return render_template("guestbook.html", data=data, title='GuestBook')
-
-
-@app.route('/addcomment')
-def addcomment():
-    return render_template("addcomment.html", title='Add Comment')
-
-
-@app.route('/thankyou', methods=['POST'])
-def thankyou():
-    if request.method == 'POST':
-        name = request.form['name']
-        comment = request.form['comment']
-        date = str(datetime.now())
-        with AzureDB() as a:
-            a.azureAddData(name, comment, date[:19])
-    return render_template('thankyou.html', title='Dziękuje', metod='POST')
-
-
-@app.route('/delete', methods=['GET', 'POST'])
-def delete():
-    index = request.form['id']
-    with AzureDB() as a:
-        a.azureDeleteData(index)
-        return render_template('thankyou.html', title='Dziękuje', metod='DELETE')
-
-
-@app.route('/edit', methods=['POST', 'GET'])
-def edit():
-    index = request.form['id']
-    with AzureDB() as a:
-        data = a.azureGetOneData(index)
-        return render_template('editcomment.html', title='Edytowanie', nick=data[1], comment=data[2], id=data[0])
-
-
-@app.route('/update', methods=['POST', 'GET'])
-def update():
-    index = request.form['id']
-    name = request.form['name']
-    text = request.form['text']
-    with AzureDB() as a:
-        a.azureUpdateData(index, name, text)
-        return render_template('thankyou.html', title='Dziękuje', metod='UPDATE')
-
-
-
-@app.route('/error_denied')
-def error_denied():
-    abort(401)
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
+
     app.run(debug=True)
+
+
+)
+
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'yourId@gmail.com', recipients = ['someone1@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
+   
+   @app.route('/')
+def hello():
+ with AzureDB() as a:
+ data = a.azureGetData()
+ return render_template("result.html", data = data)
+ 
+if __name__ == '__main__':
+ app.run(debug=True)
+
+
+config = {
+  'host':'<piac02anna>.mysql.database.azure.com',
+  'user':'<lab02>@<mydemoserver>',
+  'password':'<piac02Anna>',
+  'database':'<piac02>',
+  'client_flags': [mysql.connector.ClientFlag.SSL],
+  'ssl_ca': '/var/wwww/html/DigiCertGlobalRootG2.crt.pem'
+}
+
+# Construct connection string
+try:
+   conn = mysql.connector.connect(**config)
+   print("Connection established")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
